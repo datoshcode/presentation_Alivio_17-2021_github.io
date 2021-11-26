78a917475f5d5d110f85220a78717927d09946c3
Andrei Ovcharov <datoshcode@gmail.com>
2021-11-26 16:46:56 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,40 @@
+// Mobile navigation
+const mobileNavButton = document.querySelector('.mobile-nav-button');
+const mobileNavIcon = document.querySelector('.mobile-nav-button__icon');
+const mobileNav = document.querySelector('.mobile-nav');
+
+mobileNavButton.addEventListener('click', function () {
+  mobileNavIcon.classList.toggle('active');
+  mobileNav.classList.toggle('active');
+  document.body.classList.toggle('no-scroll');
+});
+
+// Video
+const videoBtn = document.querySelector('#video-story-button');
+const videoBtnIcon = document.querySelector('#video-story-button-icon');
+const videoOverlay = document.querySelector('#video-story-overlay');
+const videoFile = document.querySelector('#video-story');
+
+videoBtn.addEventListener('click', function () {
+
+  function toggleOverlay(event){
+    if (event.type === 'mouseleave') {
+      videoOverlay.classList.add('hidden');
+    } else {
+      videoOverlay.classList.remove('hidden');
+    }
+  }
+  
+  if (videoFile.paused) {
+    videoFile.play();
+    videoBtnIcon.src = 'images/story/pause-white.svg'; 
+    videoOverlay.onmouseleave = toggleOverlay;
+    videoOverlay.onmouseenter = toggleOverlay;
+
+  } else {
+    videoFile.pause();
+    videoBtnIcon.src = 'images/story/play-white.svg'; 
+    videoOverlay.onmouseleave = null;
+    videoOverlay.onmouseenter = null;
+  }
+});
